515158d5257ec06b958fdfd0bdf1260c6a509f5e
Alethepata <alessia.patane@gmail.com>
2024-05-27 15:21:06 +0200

    edit show

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -31,22 +31,35 @@ const show = (req, res) => {
     res.format({
         html: () => {
             let html;
-
+            if (post) {
                 html += `
-            <div>
-               <h1>${post.title}</h1>
-               <img width="500" src="/imgs/posts/${post.image}" alt="${post.title}">
-               <p>${post.content}</p>
-               <ul>
-            `;
-                post.tags.forEach(tag => html += `<li>${tag}</li>`);
-                html += '</ul></div>';
+                <div>
+                   <h1>${post.title}</h1>
+                   <img width="500" src="/imgs/posts/${post.image}" alt="${post.title}">
+                   <p>${post.content}</p>
+                   <ul>
+                `;
+                    post.tags.forEach(tag => html += `<li>${tag}</li>`);
+                    html += '</ul></div>';
+            } else {
+                html += `
+                <div>
+                   <h1>Non trovato</h1>
+                </div>`
+            }
                 
             res.send(html);
     
         },
         json: () => {
-            res.json(post);
+            if (post) {
+                res.json(post); 
+            } else {
+                res.status(404).json({
+                    error: 'Not Found',
+                    describe: `Non esiste una pizza ${slug}`
+                });
+            }
         }
     })
 }
