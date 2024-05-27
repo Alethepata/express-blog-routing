b3aac304fb22c25f43023ee7e9b708b10043e988
Alethepata <alessia.patane@gmail.com>
2024-05-27 14:56:43 +0200

    add routers | index

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const { controllerPosts } = require('./controllers/postsController');
+const postsRouter = require('./routers/posts.js');
+
 
 app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.send(`<h1>Benvenuto nel mio blog</h1>`)
 });
-app.get('/posts', controllerPosts);
+
+app.use('/posts', postsRouter);
 
 app.listen(port, () => {
     console.log('Server Online')
--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,28 +1,30 @@
 const posts = require('../db/posts.js');
 
+const index = (req, res) => { 
+    res.format({
+        html: () => {
+            let html;
+            posts.forEach(post => {
+                html += `
+            <div>
+               <h1>${post.title}</h1>
+               <img width="500" src="imgs/posts/${post.image}" alt="${post.title}">
+               <p>${post.content}</p>
+               <ul>
+            `;
+                post.tags.forEach(tag => html += `<li>${tag}</li>`);
+                html += '</ul></div>';
+                
+            })
+            res.send(html);
+    
+        },
+        json: () => {
+            res.json(posts);
+        }
+    })
+}
+
 module.exports = {
-    controllerPosts: (req, res) => { 
-        res.format({
-            html: () => {
-                let html;
-                posts.forEach(post => {
-                    html += `
-                <div>
-                   <h1>${post.title}</h1>
-                   <img width="500" src="imgs/posts/${post.image}" alt="${post.title}">
-                   <p>${post.content}</p>
-                   <ul>
-                `;
-                    post.tags.forEach(tag => html += `<li>${tag}</li>`);
-                    html += '</ul></div>';
-                    
-                })
-                res.send(html);
-        
-            },
-            json: () => {
-                res.json(posts);
-            }
-        })
-    }
+    index,
 }
