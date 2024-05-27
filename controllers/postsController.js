01f28bb40b199fa1a75d1890224e1c1c8f912873
Alethepata <alessia.patane@gmail.com>
2024-05-27 15:11:06 +0200

    add show

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,6 +1,6 @@
 const posts = require('../db/posts.js');
 
-const index = (req, res) => { 
+const index = (req, res) => {
     res.format({
         html: () => {
             let html;
@@ -23,8 +23,35 @@ const index = (req, res) => {
             res.json(posts);
         }
     })
+};
+
+const show = (req, res) => {
+    const slug = req.params.slug;
+    const post = posts.find(post => post.slug === slug);
+    res.format({
+        html: () => {
+            let html;
+
+                html += `
+            <div>
+               <h1>${post.title}</h1>
+               <img width="500" src="/imgs/posts/${post.image}" alt="${post.title}">
+               <p>${post.content}</p>
+               <ul>
+            `;
+                post.tags.forEach(tag => html += `<li>${tag}</li>`);
+                html += '</ul></div>';
+                
+            res.send(html);
+    
+        },
+        json: () => {
+            res.json(post);
+        }
+    })
 }
 
 module.exports = {
     index,
+    show
 }
